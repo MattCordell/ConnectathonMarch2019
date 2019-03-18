8ff5c0fef90375d2beb53725f0581dff6570be38
Matt Cordell <matthew.cordell@nehta.gov.au>
2019-03-19 09:36:11 +1000

    Add functional Form App (looks dirty but works)

---
+++ WindowsFormsApp2/Form1.Designer.cs
@@ -0,0 +1,171 @@
+﻿namespace WindowsFormsApp2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.patientBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.patientBindingSource)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer1.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer1.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.splitContainer1.Name = "splitContainer1";
+            this.splitContainer1.Orientation = System.Windows.Forms.Orientation.Horizontal;
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.button1);
+            this.splitContainer1.Panel1.Controls.Add(this.label2);
+            this.splitContainer1.Panel1.Controls.Add(this.textBox2);
+            this.splitContainer1.Panel1.Controls.Add(this.label1);
+            this.splitContainer1.Panel1.Controls.Add(this.textBox1);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.dataGridView1);
+            this.splitContainer1.Size = new System.Drawing.Size(1624, 985);
+            this.splitContainer1.SplitterDistance = 519;
+            this.splitContainer1.SplitterWidth = 8;
+            this.splitContainer1.TabIndex = 0;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(654, 137);
+            this.label2.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(49, 25);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Foo";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(764, 125);
+            this.textBox2.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(196, 31);
+            this.textBox2.TabIndex = 2;
+            this.textBox2.Text = "Foo";
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(658, 63);
+            this.label1.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(112, 25);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Drug Filter";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(768, 52);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(196, 31);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.Text = "1073941000168107";
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.DisplayedCells;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dataGridView1.Location = new System.Drawing.Point(0, 0);
+            this.dataGridView1.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.Size = new System.Drawing.Size(1624, 458);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // patientBindingSource
+            // 
+            this.patientBindingSource.DataSource = typeof(WindowsFormsApp2.Patient);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(764, 207);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(200, 65);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1624, 985);
+            this.Controls.Add(this.splitContainer1);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel1.PerformLayout();
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.patientBindingSource)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.BindingSource patientBindingSource;
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ WindowsFormsApp2/Form1.cs
@@ -0,0 +1,208 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using Hl7.Fhir.Model;
+using Hl7.Fhir.Rest;
+
+
+namespace WindowsFormsApp2
+{
+
+    public partial class Form1 : Form
+    {
+        List<Patient> data = new List<Patient>();
+
+        //string drugFilter = "*";
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            data = LoadPatientData();
+            var dt = new DataTable();
+
+
+
+            //var bindingList = new BindingList<Patient>(data);           
+            //bindingList.Where<Patient>(p => p.drugId.Equals(1073941000168107));       
+            //var source = new BindingSource(bindingList, null);         
+            //dataGridView1.DataSource = data.Where<Patient>(p => p.
+
+
+            dataGridView1.DataSource = data;
+
+            
+            
+
+
+            dataGridView1.Show();
+            
+            
+        }
+
+        public List<Patient> LoadPatientData()
+        {
+            var dataFile = @"C:\Users\Matthew.Cordell\Desktop\ConnectData.txt";
+            var data = File.ReadAllLines(dataFile);
+            var d2 = new List<Patient>();
+
+            foreach (var item in data)
+            {
+                var x = item.Split('\t');
+
+                var patient = new Patient();
+                patient.firstName = x[0];
+                patient.lastName = x[1];
+                patient.sex = x[2];
+                patient.dob = x[3];
+                patient.drugId = x[4];
+                patient.drugName = x[5];
+                patient.disorderId = x[6];
+                patient.disorderName = x[7];
+
+                d2.Add(patient);
+            }
+            return d2;
+        }
+
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var drugFilter = GenerateDrugFilter();
+            
+            List<Patient> result = data.Where<Patient>(p => drugFilter.Contains(p.drugId)).ToList();
+         
+
+            dataGridView1.DataSource = result;
+        }
+
+        private List<string> GenerateDrugFilter()
+        {
+            var ecl = textBox2.Text;
+            const string Endpoint = "https://ontoserver.csiro.au/stu3-latest";
+            string ValueSetURL = "http://snomed.info/sct?fhir_vs=ecl/"+ ecl;
+
+            //ExpandECL for Box2
+            var client = new FhirClient(Endpoint);           
+            var url = new FhirUri(ValueSetURL);
+            var result = client.ExpandValueSet(url);
+
+            var ids = new List<string>();
+
+            foreach (var item in result.Expansion.Contains)
+            {
+                ids.Add(item.Code);
+            }
+
+            var translations = new List<string>();
+
+            foreach (var id in ids)
+            {
+                //Reverse Translate
+                var revsertTranslateParameters = new Parameters
+                {
+                    Parameter = new List<Parameters.ParameterComponent>
+               {
+                  new Parameters.ParameterComponent
+                  {
+                    Name = "url",
+                    Value = new FhirUri("http://snomed.info/sct?fhir_cm=281000036105")
+                  },
+                  new Parameters.ParameterComponent
+                  {
+                    Name = "system",
+                    Value = new FhirUri("http://snomed.info/sct")
+                  },
+                  new Parameters.ParameterComponent
+                  {
+                    Name = "code",
+                    Value = new FhirString(id)
+                  },
+                  new Parameters.ParameterComponent
+                  {
+                    Name = "target",
+                    Value = new FhirUri("http://snomed.info/sct")
+                  },
+                  new Parameters.ParameterComponent
+                  {
+                    Name = "reverse",
+                    Value = new FhirBoolean(true)
+                  }
+                 }
+                };
+
+
+                var transResult = (Parameters)client.TypeOperation<ConceptMap>("translate", revsertTranslateParameters);
+                //if there's a map
+                //if (transResult.Parameter[0].Value.Equals("true"))
+                //{
+                    foreach (var match in transResult.Parameter.Where<Parameters.ParameterComponent>(e => e.Name == "match"))
+                    {
+                        //var coding = match.Part.Where<Parameters.ParameterComponent>(e => e.Name == "concept");
+                        //This workds
+                        //var coding1 = match.Part[1].Value;
+                        var valueCoding = match.Part.Where<Parameters.ParameterComponent>(p => p.Name.Equals("concept")).FirstOrDefault();
+
+                        var coding = (Coding)valueCoding.Value;
+
+                        translations.Add(coding.Code);                        
+                    }
+                //}
+
+            }
+
+            //Expand the AMT ingredients
+            string AMT_ecl = "http://snomed.info/sct?fhir_vs=ecl/<<30425011000036101:<<762951001=(";
+
+
+            foreach (var item in translations)
+            {
+                AMT_ecl = AMT_ecl + item + " OR ";
+            }
+            //close the expression
+            AMT_ecl = AMT_ecl + "123456 OR 123456)";
+
+            var AMT_url = new FhirUri(AMT_ecl);
+            var AMTs = client.ExpandValueSet(AMT_url);
+
+            var filtered = new List<string>();
+            foreach (var item in AMTs.Expansion.Contains)
+            {
+                filtered.Add(item.Code);
+            }
+
+
+            return filtered;
+        }
+    }
+
+    public class Patient
+    {
+        public string firstName { get; set; }
+        public string lastName { get; set; }
+        public string sex { get; set; }
+        public string dob { get; set; }
+        public string drugId { get; set; }
+        public string drugName { get; set; }
+        public string disorderId { get; set; }
+        public string disorderName { get; set; }
+    }
+
+}
